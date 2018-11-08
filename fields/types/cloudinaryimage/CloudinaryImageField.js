d1dda78c30be04d97ab00cd50eb833e85cbd8499
TerryChan <terrylau@4d.com.hk>
2018-11-08 19:57:17 +0800

    add encodeURIComponent

--- fields/types/cloudinaryimage/CloudinaryImageField.js
+++ fields/types/cloudinaryimage/CloudinaryImageField.js
@@ -7,7 +7,7 @@ work out whether we're going to support deleting through the UI.
 import React, { PropTypes } from 'react';
 import Field from '../Field';
 import cloudinaryResize from '../../../admin/client/utils/cloudinaryResize';
-// import downloadImage from '../../../admin/client/utils/downloadImage';
+import downloadImage from '../../../admin/client/utils/downloadImage';
 import { Button, FormField, FormInput, FormNote } from '../../../admin/client/App/elemental';
 
 import ImageThumbnail from '../../components/ImageThumbnail';
@@ -283,8 +283,12 @@ module.exports = Field.create({
 					// }}>
 					// 	Original Image
 					// </Button> : null
-					<a href={this.getImageSource({ resizable: false })} download={filename}>
-						Download Original Image
+					<a 
+						href={`data:application/octet-stream, ${encodeURIComponent(this.getImageSource({ resizable: false }))}`}
+						target="_blank"
+						download={filename}
+					>
+						Original Image
 					</a> : null
 				}
 				<Button onClick={this.triggerFileBrowser}>
