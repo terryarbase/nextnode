da621fb245011a70bac578e9e89fdc1f5c1da705
funglee <funglee@4d.com.hk>
2018-11-08 15:03:10 +0800

    add missing layout scss

---
+++ admin/client/App/components/Navigation/AdminMenu/SubMenuNav.js
@@ -0,0 +1,95 @@
+/**
+ * The secondary navigation links to inidvidual lists of a section
+ */
+
+import React from 'react';
+import { connect } from 'react-redux';
+import { Container } from '../../../elemental';
+
+import {
+	setActiveList,
+} from '../../../screens/List/actions/active';
+import SubMenuNavItem from './SubMenuNavitem';
+
+var SubMenuNav = React.createClass({
+	displayName: 'SubMenuNav',
+	propTypes: {
+		currentListKey: React.PropTypes.string,
+		lists: React.PropTypes.array.isRequired,
+	},
+	getInitialState () {
+		return {
+			isActive: false,
+		}
+	},
+	setState() {
+		this.setState({
+			isActive: this.props.currentListKey && this.props.currentListKey === list.path,
+		});
+	},
+	// Render the navigation
+	renderNavigation (lists) {
+		const navigation = Object.keys(lists).map((key) => {
+			const list = lists[key];
+			// Get the link and the classname
+			const href = list.external ? list.path : `${Keystone.adminPath}/${list.path}`;
+			const isActive = this.props.currentListKey && this.props.currentListKey === list.path;
+			const className = isActive ? 'active' : null;
+			const onClick = (evt) => {
+				// If it's the currently active navigation item and we're not on the item view,
+				// clear the query params on click
+				if (isActive && !this.props.itemId) {
+					evt.preventDefault();
+					this.props.dispatch(
+						setActiveList(this.props.currentList, this.props.currentListKey)
+					);
+				}
+			};
+
+			return (
+				<SubMenuNavItem
+					active={isActive}
+					key={list.path}
+					path={list.path}
+					className={className}
+					href={href}
+					onClick={onClick}
+				>
+					{list.label}
+				</SubMenuNavItem>
+			);
+		});
+
+		return (
+			navigation
+		);
+	},
+	render () {
+		const { parentActive, isHover } = this.props;
+		return (
+			parentActive || isHover ? (
+				<ul className="sub-menu-list">
+					{this.renderNavigation(this.props.lists)}
+				</ul>
+			) : null
+		);
+
+		// const {parentActive} = this.props;
+		// var classNames = ['sub-menu-list'];
+		// if (!sectionActive) {
+		// 	classNames = [...classNames, 'hover'];
+		// }
+
+		// return (
+		// 	<ul className={classNames.join(' ')}>
+		// 		{this.renderNavigation(this.props.lists)}
+		// 	</ul>
+		// );
+	},
+});
+
+module.exports = connect((state) => {
+	return {
+		currentList: state.lists.currentList,
+	};
+})(SubMenuNav);
---
+++ admin/client/App/components/Navigation/AdminMenu/SubMenuNavitem.js
@@ -0,0 +1,40 @@
+/**
+ * A navigation item of the secondary navigation
+ */
+
+import React from 'react';
+import { Link } from 'react-router';
+
+const SubMenuNavItem = React.createClass({
+	displayName: 'SubMenuNavItem',
+	propTypes: {
+		children: React.PropTypes.node.isRequired,
+		className: React.PropTypes.string,
+		href: React.PropTypes.string.isRequired,
+		onClick: React.PropTypes.func,
+		path: React.PropTypes.string,
+		title: React.PropTypes.string,
+    },
+	render () {
+		const {active} = this.props;
+		var classNames = ['sub-menu-item'];
+		if (active) {
+			classNames = [...classNames, 'active'];
+		}
+		return (
+			<li className={classNames.join(' ')} data-list-path={this.props.path}>
+				<Link
+					className="sub-menu-link"
+					to={this.props.href}
+					onClick={this.props.onClick}
+					title={this.props.title}
+					tabIndex="-1"
+				>
+					{this.props.children}
+				</Link>
+			</li>
+		);
+	},
+});
+
+module.exports = SubMenuNavItem;
