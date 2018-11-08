f5a981172e42b7131ab58daf7f15453a045f6572
TerryChan <terrylau@4d.com.hk>
2018-11-08 18:56:50 +0800

    fix default option of { height = 90, resizable = true }

--- fields/types/cloudinaryimage/CloudinaryImageField.js
+++ fields/types/cloudinaryimage/CloudinaryImageField.js
@@ -86,7 +86,8 @@ module.exports = Field.create({
 			? this.state.userSelectedFile.name
 			: `${public_id}.${format} (${width}×${height})`;
 	},
-	getImageSource ({ height = 90, resizable = true }) {
+	getImageSource (options) {
+		const { height = 90, resizable = true } = options;
 		// TODO: This lets really wide images break the layout
 		let src;
 		if (this.hasLocal() || !resizable) {
