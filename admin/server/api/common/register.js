d3d73b1812fb483e7806ff2e5c337e611aa30a86
Terry Chan <terry@madmadgroup.com>
2019-03-30 14:14:07 +0800

    add delegated message state

--- admin/server/api/common/register.js
+++ admin/server/api/common/register.js
@@ -6,12 +6,13 @@
 */
 const _ = require('lodash');
 
-module.exports = function (req, res) {
+module.exports = async (req, res) => {
 	const keystone = req.keystone;
 	const registerFields = [
 		'browserDeviceToken',
 		'enabledBrowserPush',
 	];
+	const userList = keystone.list('users');
 	// specific the fields to be saved
 	const options = {
 		ignoreNoEdit: true,
@@ -20,7 +21,23 @@ module.exports = function (req, res) {
 	};
 	// obtain the fields to be save from request data user pairs to construct the array into an object
 	const data = _.fromPairs(_.map(registerFields, f => [f, req.body[f]]));
-	keystone.list('users').updateItem(req.user, data, options, function (err) {
+
+	// remove all of the same browser DeviceToken
+	// in case use the same kind of browser to login different account
+	if (data.browserDeviceToken) {
+		await userList.model.update({
+			browserDeviceToken: data.browserDeviceToken,
+			_id: {
+				$ne: req.user._id,
+			},
+		}, {
+			browserDeviceToken: null,
+		}, {
+			multi: true
+		}).exec();
+	}
+
+	userList.updateItem(req.user, data, options, function (err) {
 		if (err) {
 			var status = err.error === 'validation errors' ? 400 : 500;
 			var error = err.error === 'database error' ? err.detail : err;
