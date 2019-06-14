b848ddf90df52fcd4b8e4d9555d6a9afa1f25c70
funglee <funglee@4d.com.hk>
2019-06-14 20:46:48 +0800

    fix admin menu UI bug

--- admin/client/App/components/Navigation/AdminMenu/AdminMenuNav.js
+++ admin/client/App/components/Navigation/AdminMenu/AdminMenuNav.js
@@ -22,17 +22,21 @@ var AdminMenuNav = React.createClass({
 	},
 	getInitialState () {
 		return {
+			init: true,
 			selectedIndex: -1,
 			menuState: "top",
+			menuTopInBody: 0,
 			lastScrollTop: 0,
 			adminMenuStyle: {
-				position: "absolute",
+				position: "relative",
 				top: 0,
-			}
+			},
 		};
 	},
 	// Handle resizing, hide this navigation on mobile (i.e. < 768px) screens
 	componentDidMount () {
+		this.doUpdate = true;
+		this.menuChanged = false;
 		this.handleResize();
 		window.addEventListener('resize', this.handleResize);
 		window.addEventListener('scroll', this.handleScroll);
@@ -40,12 +44,41 @@ var AdminMenuNav = React.createClass({
 	componentWillUnmount () {
 		window.removeEventListener('resize', this.handleResize);
 	},
+	componentDidUpdate () {
+		if (this.doUpdate) {
+			this.updateMenuPosition();
+		}
+	},
 	handleResize () {
-		this.setState({
+		let state = {
 			navIsVisible: window.innerWidth >= 768,
-		});
+		};
+
+		if (this.menu) {
+			const menu = this.menu;
+			const menuHeight = menu.offsetHeight;
+			const scrollTop = document.documentElement.scrollTop || window.pageYOffset;
+			const windowHeight = window.innerHeight;
+
+			if (windowHeight > menuHeight) {
+				state = {
+					...state,
+					action: "resize change 'top' state",
+					menuState: "top",
+					menuTopInBody: scrollTop,
+					adminMenuStyle: {
+						position: "fixed",
+						top: 0,
+					},
+				}
+			}
+		}
+
+		this.setState(state);
 	},
 	handleScroll () {
+		if (this.menuChanged) return;
+
 		const menu = this.menu;
 		const menuTop = menu.offsetTop;
 		const menuHeight = menu.offsetHeight;
@@ -53,55 +86,201 @@ var AdminMenuNav = React.createClass({
 		const windowHeight = window.innerHeight;
 		const scrollTop = document.documentElement.scrollTop || window.pageYOffset;
 		const scrollBottom = scrollTop + windowHeight;
-		const isScrollDown = (scrollTop > this.state.lastScrollTop);
+		const lastScrollTop = this.state.lastScrollTop;
+		const lastScrollBottom = lastScrollTop + windowHeight;
+		const isScrollDown = (scrollTop > lastScrollTop);
 		const bodyHeight = window.document.body.offsetHeight;
-		
-		if (menuHeight <= windowHeight || bodyHeight <= menuHeight) return;
-		// Handle menu scrolling
-		if (isScrollDown) {
-			// scroll down
-			if (this.state.menuState === "top") {
-				this.setState({
-					menuState: "middle",
-					adminMenuStyle: {
-						position: "absolute",
-						top: scrollTop,
-					}
-				});
-			} else if (scrollBottom >= menuBottom) {
-				this.setState({
-					menuState: "bottom",
-					adminMenuStyle: {
-						position: "fixed",
-						bottom: 0,
-					}
-				});
-			}
-		} else {
-			// scroll up
-			if (this.state.menuState === "bottom") {
-				this.setState({
-					menuState: "middle",
-					adminMenuStyle: {
-						position: "absolute",
-						top: scrollBottom - menuHeight,
-					}
-				});
-			} else if (scrollTop <= menuTop) {
-				this.setState({
+
+		// console.log('>>> debug', {
+		// 	menuTop,
+		// 	menuHeight,
+		// 	menuBottom,
+		// 	windowHeight,
+		// 	scrollTop,
+		// 	scrollBottom,
+		// 	lastScrollTop,
+		// 	lastScrollBottom,
+		// 	isScrollDown,
+		// 	bodyHeight,
+		// })
+
+		let state = {
+			lastScrollTop: scrollTop <= 0 ? 0 : scrollTop,
+		}
+
+		if (this.state.init) {
+			const menuMaxTop = bodyHeight - menuHeight;
+			if (scrollTop <= menuMaxTop) {
+				state = {
+					...state,
+					action: "refresh init 'top' state",
+					init: false,
 					menuState: "top",
+					menuTopInBody: scrollTop,
 					adminMenuStyle: {
 						position: "fixed",
 						top: 0,
+					},
+				}
+			} else {
+				state = {
+					...state,
+					action: "refresh init 'bottom' state",
+					init: false,
+					menuState: "bottom",
+					menuTopInBody: menuMaxTop,
+					adminMenuStyle: {
+						position: "fixed",
+						bottom: 0,
+					},
+				}
+			}
+			this.setState(state);
+			return;
+		}
+
+		if (menuHeight >= windowHeight && bodyHeight <= menuHeight) {
+			state = {
+				menuState: "top",
+				adminMenuStyle: {
+					position: "relative",
+					top: 0,
+				},
+			}
+			this.setState(state);
+			return;
+		};
+
+		if (isScrollDown) {
+			/* scroll down */
+			// handle 'top' state
+			if (this.state.menuState === "top") {
+				if (menuHeight <= windowHeight) {
+					// keep 'top' state
+					state = {
+						...state,
+						action: "[down] keep 'top' state",
+						menuState: "top",
+						adminMenuStyle: {
+							position: "fixed",
+							top: 0,
+						}
 					}
-				});
+				} else {
+					// change 'middle' state
+					state = {
+						...state,
+						action: "[down] change 'middle' state",
+						menuState: "middle",
+						adminMenuStyle: {
+							position: "absolute",
+							top: this.state.menuTopInBody,
+						}
+					}
+				}
+			}
+
+			// handle 'middle' state
+			if (this.state.menuState === "middle") {
+				if (scrollBottom >= menuBottom) {
+					// scroll until menu bottom, change 'bottom' state
+					state = {
+						...state,
+						action: "[down] change 'bottom' state",
+						menuTopInBody: scrollBottom - menuHeight,
+						menuState: "bottom",
+						adminMenuStyle: {
+							position: "fixed",
+							bottom: 0,
+						}
+					}
+				}
+			}
+
+			// handle 'bottom' state
+			if (this.state.menuState === "bottom") {
+				// set menuTopInBody
+				state = {
+					...state,
+					action: "[down] set menuTopInBody",
+					menuTopInBody: scrollBottom - menuHeight,
+				}
 			}
 		}
-		this.setState({
-			lastScrollTop: scrollTop <= 0 ? 0 : scrollTop,
-		});
+
+		if (!isScrollDown) {
+			/* scroll up */
+			// handle 'bottom' state
+			if (this.state.menuState === "bottom") {
+				// change 'middle' state
+				const top = scrollBottom - menuHeight;
+				state = {
+					...state,
+					action: "[up] change 'middle' state",
+					menuState: "middle",
+					adminMenuStyle: {
+						position: "absolute",
+						top: top < 0 ? 0 : top,
+						// top: this.state.menuTopInBody,
+					}
+				}
+			}
+
+			// handle 'middle' state
+			if (this.state.menuState === "middle") {
+				// change 'top' state
+				if (scrollTop <= menuTop) {
+					state = {
+						...state,
+						action: "[up] change 'top' state",
+						menuTopInBody: scrollTop,
+						menuState: "top",
+						adminMenuStyle: {
+							position: "fixed",
+							top: 0,
+						}
+					}
+				}
+			}
+
+			// handle 'top' state
+			if (this.state.menuState === "top") {
+				// set menuTopInBody
+				state = {
+					...state,
+					action: "[up] set menuTopInBody",
+					menuTopInBody: scrollTop,
+				}
+			}
+		}
+
+		this.setState(state);
+	},
+	updateMenuPosition () {
+		const menu = this.menu;
+		const menuHeight = menu.offsetHeight;
+		const windowHeight = window.innerHeight;
+		const bodyHeight = window.document.body.offsetHeight;
+		
+		let state = {};
+
+		if (menuHeight <= windowHeight || bodyHeight <= menuHeight) {
+			state = {
+				menuState: "top",
+				adminMenuStyle: {
+					position: "relative",
+					top: 0,
+				},
+			}
+		}
+		
+		this.doUpdate = false;
+		this.menuChanged = false;
+		this.setState(state);
 	},
 	changeMenu (idx) {
+		this.doUpdate = true;
+		this.menuChanged = true;
 		this.setState({
 			selectedIndex: idx,
 		});
@@ -156,6 +335,7 @@ var AdminMenuNav = React.createClass({
 
 	render () {
 		if (!this.state.navIsVisible) return null;
+		// console.log('>>> state', this.state);
 		return (
 			<div ref={(r) => this.menu = r} className="admin-menu" style={this.state.adminMenuStyle}>
 				{this.renderBrand()}
