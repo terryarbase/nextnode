1a6d2a3618fef1b97e8a303e7a2e234667b0e477
funglee <funglee@4d.com.hk>
2020-06-02 15:58:14 +0800

    update mongoose validation error in api to more clearly

--- admin/server/api/item/update.js
+++ admin/server/api/item/update.js
@@ -29,8 +29,16 @@ module.exports = function (req, res) {
 		req.list.updateItem(item, newData, options, function (err) {
 			if (err) {
 				var status = err.error === 'validation errors' ? 400 : 500;
-				var error = err.error === 'database error' ? err.detail : err;
-				return res.apiError(status, err);
+				let error = err;
+				if (err.error === 'database error' && err.detail && err.detail.errors) {
+					error = {
+						...err,
+						detail: _.map(err.detail.errors, e => {
+							return `[${e.path}] : ${e.message}`;
+						}).join(', ')
+					}
+				}
+				return res.apiError(status, error);
 			}
 			// Reload the item from the database to prevent save hooks or other
 			// application specific logic from messing with the values in the item
