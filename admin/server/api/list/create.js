9ad7af336bbc0665f47104660ebb3cdae13b7a60
funglee <funglee@4d.com.hk>
2020-07-15 12:38:08 +0800

    add comment

--- admin/server/api/list/create.js
+++ admin/server/api/list/create.js
@@ -8,7 +8,15 @@ module.exports = async function (req, res) {
 	const { body, locales, list: { options: { multilingual, nolist }, fields } } = req;
 	req.list.prepareDataPermission(req, body);
 	let newData = req.list.prepareCorrectParam(body);
-	
+	/*
+	** How to check the field permission like this
+	{
+		redeemAt_date: '2020-07-10',
+		redeemAt_time: '6:14:10 pm',
+		redeemAt_tzOffset: '+08:00',
+	}
+	*/
+	// newData = _.pick(newData, req.permissionAllowFields);
 	const item = new req.list.model();
 	const options = {
 		files: req.files,
@@ -27,7 +35,7 @@ module.exports = async function (req, res) {
 
 	const [bcError] = await req.list.applyHook('beforeCreate', item, newData, { req });
 	if (bcError) return res.apiError(406, 'database error', bcError);
-
+	
 	req.list.updateItem(item, newData, options, async function (err) {
 		if (err) {
 			var status = err.error === 'validation errors' ? 400 : 500;
